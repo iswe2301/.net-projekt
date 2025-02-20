a8eba954c6382cd63a87cb6ddb24b573d49646f8
Isa Westling <isawestling.blogg@hotmail.com>
2025-02-20 22:14:07 +0100

    Ändrat standardrutt för att peka på produktkontroller istället för hemkontroller

--- Program.cs
+++ Program.cs
@@ -47,7 +47,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Product}/{action=Home}/{id?}")
     .WithStaticAssets();
 
 app.MapRazorPages()
